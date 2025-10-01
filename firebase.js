52c198c2effcc5cc4ff5aa7b5bcd0686e90ffaea
Prince_Ali <117987889+Izzulliminati@users.noreply.github.com>
2025-10-01 21:45:00 +0700

    Initial commit project bakmi

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBo8VuMP2r8zhCgdntPKXy0_jgSGecTDzE",
+  authDomain: "qr-order-system-b2993.firebaseapp.com",
+  projectId: "qr-order-system-b2993",
+  storageBucket: "qr-order-system-b2993.appspot.com",
+  messagingSenderId: "349306459617",
+  appId: "1:349306459617:web:51e471d2a9538f9107eea0",
+  measurementId: "G-NB68E6CX0K"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// 🔹 Export auth untuk login
+export const auth = getAuth(app);
+export const db = getFirestore(app);
